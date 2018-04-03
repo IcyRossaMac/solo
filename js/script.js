1876b43a1823f450b747cd2621e3e77a3664111c
Rossa McLaughlin <IcyRossa@gmail.com>
2018-04-03 16:52:33 +0100

    Portfolio CSS added

--- js/script.js
+++ js/script.js
@@ -66,6 +66,18 @@ $(window).on('load', function () {
     });
 });
 
+/* Magnifier */
+
+$(function () {
+    $('#portfolio-wrapper').magnificPopup({
+        delegate: 'a', // child items selector, by clicking on it popup will open
+        type: 'image',
+        gallery:{
+            enabled:true
+        }
+    });
+});
+
 
 
 
